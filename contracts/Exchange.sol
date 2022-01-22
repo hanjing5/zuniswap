3bd68fcf00aef160d287dc39161dac5db1d07ee7
Han Jing <hanjing@berkeley.edu>
2022-01-21 18:56:41 -0800

    add withdraw functions

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -28,4 +28,26 @@ contract Exchange {
 
         return inputReserve * 1000 / outputReserve;
     }
+
+    function getAmount(
+        uint256 inputAmount,
+        uint256 inputReserve,
+        uint256 outputReserve
+    )  private pure returns (uint256) {
+        require(inputReserve > 0 && outputReserve > 0, "invalid reserves");
+        return (inputAmount * outputReserve) / (inputReserve + inputAmount);
+    }
+
+    function getTokenAmount(uint256 _ethSold) public view returns (uint256) {
+        require(_ethSold > 0, "ethSold is too small");
+        uint256 tokenReserve = getReserve();
+
+        return getAmount(_ethSold, address(this).balance, tokenReserve);
+    }
+    function getEthAmount(uint256 _tokenSold) public view returns (uint256) {
+        require(_tokenSold > 0, "tokenSold is too small");
+        uint256 tokenReserve = getReserve();
+
+        return getAmount(_tokenSold, tokenReserve, address(this).balance);
+    }
 }   
